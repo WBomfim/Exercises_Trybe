4721760792b50a7a688c41eb43c5c084271112c2
WBomfim <willianbomfim@hotmail.com>
2022-03-14 21:38:15 -0300

    Adicionado exercícios fundamentos/8.4

---
+++ fundamentos/bloco_08_higher-order-functions-do-javascript-ES6/dia_04_javascript ES6-higher-order-functions-reduce/exercicio_06.js
@@ -0,0 +1,9 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  return students.map((element, index) => 
+  ({ name: `${element}`, average: grades[index].reduce((acc, curr) => acc + curr) / grades[index].length }))
+}
+
+console.log(studentAverage());
