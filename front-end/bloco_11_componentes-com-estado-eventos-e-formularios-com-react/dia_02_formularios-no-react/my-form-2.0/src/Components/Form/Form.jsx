fbb2e8e6f697bbc8e5c0536ab8f0a24439220ea0
WBomfim <willianbomfim@hotmail.com>
2022-04-16 18:36:35 -0300

    Alterada a função chengeSubmit e adicionado a função showInformationForm para renderizar na tela as informações do formulário

--- front-end/bloco_11_componentes-com-estado-eventos-e-formularios-com-react/dia_02_formularios-no-react/my-form-2.0/src/Components/Form/Form.jsx
+++ front-end/bloco_11_componentes-com-estado-eventos-e-formularios-com-react/dia_02_formularios-no-react/my-form-2.0/src/Components/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PersonalForm from "../PersonalForm/PersonalForm";
 import verifyForm from "../../helpers/verifyForm";
+import './Form.css';
 
 class Form extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class Form extends Component {
       estado: "",
       tipo: "",
       errors: "",
+      submit: false
     }
   }
 
@@ -31,18 +33,43 @@ class Form extends Component {
     this.setState({ [name]: value });
   }
 
+  showInformationsForm = () => {
+    const { name, email, cpf, endereco, cidade, estado, tipo } = this.state;
+    return (
+      <div className="show-form">
+        <p>Nome: {name}</p>
+        <p>Email: {email}</p>
+        <p>CPF: {cpf}</p>
+        <p>Endereço: {endereco}</p>
+        <p>Cidade: {cidade}</p>
+        <p>Estado: {estado}</p>
+        <p>Tipo: {tipo}</p>
+      </div>
+    )
+  }
+
+  changeSubmit = () => {
+    const { errors } = this.state;
+    if (errors.length === 0) {
+      this.setState({ submit: true });
+    } else {
+      this.setState({ submit: false });
+    }
+  }
+
   validateForm = (event) => {
     event.preventDefault();
-    this.setState({ errors: verifyForm(this.state) });
+    this.setState({ errors: verifyForm(this.state) }, () => this.changeSubmit());
   }
 
   render() {
    return (
-     <div>
+     <div className="formAll">
        <form action="">
           <PersonalForm value={this.state} handleChange={this.handleChange} onBlurHandler={this.onBlurHandler} />
-          <button type="submit" onClick={(e) => this.validateForm(e)}>Enviar</button>
+          <button type="submit" onClick={(event) => this.validateForm(event)}>Enviar</button>
        </form>
+        {this.state.submit && <>{this.showInformationsForm()}</>}
      </div>
    )
   }
