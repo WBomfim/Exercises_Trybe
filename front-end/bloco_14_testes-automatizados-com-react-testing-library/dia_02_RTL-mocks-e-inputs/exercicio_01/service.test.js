cbdbcacd7a92fb02b59d827aff040478633959e5
WBomfim <willianbomfim@hotmail.com>
2022-05-07 21:53:10 -0300

    test: Adicionado mas testes com mock para a função randomNumber

--- front-end/bloco_14_testes-automatizados-com-react-testing-library/dia_02_RTL-mocks-e-inputs/exercicio_01/service.test.js
+++ front-end/bloco_14_testes-automatizados-com-react-testing-library/dia_02_RTL-mocks-e-inputs/exercicio_01/service.test.js
@@ -2,11 +2,18 @@ const service = require('./service');
 
 
 describe('Teste das funções do arquivo service', () => {
-  test("Verifica se a função randomNumber foi chamada, qual seu retorno e quantas vezes foi chamada realizando um mock", () => {
+  it('Verifica se a função randomNumber foi chamada, qual seu retorno e quantas vezes foi chamada realizando um mock', () => {
     service.randomNumber = jest.fn().mockReturnValue(10);
   
     expect(service.randomNumber()).toBe(10);
     expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  it('Verifica se a função randomNumber foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros realizando um mock', () => {
+    service.randomNumber = jest.fn().mockImplementationOnce((num1, num2) => num1 / num2);
+
+    expect(service.randomNumber(4, 2)).toBe(2);
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
   });
 });
