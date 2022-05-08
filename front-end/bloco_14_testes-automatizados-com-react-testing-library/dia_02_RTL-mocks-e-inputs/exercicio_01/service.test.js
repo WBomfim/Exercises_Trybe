4be826b5e1b2335285d93b33295e6fe6da4d96e3
WBomfim <willianbomfim@hotmail.com>
2022-05-07 22:06:03 -0300

    test: Implementado mais testes com mock para a função randomNumber

--- front-end/bloco_14_testes-automatizados-com-react-testing-library/dia_02_RTL-mocks-e-inputs/exercicio_01/service.test.js
+++ front-end/bloco_14_testes-automatizados-com-react-testing-library/dia_02_RTL-mocks-e-inputs/exercicio_01/service.test.js
@@ -10,10 +10,28 @@ describe('Teste das funções do arquivo service', () => {
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
   });
 
-  it('Verifica se a função randomNumber foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros realizando um mock', () => {
+  it('Verifica se a função randomNumber foi chamada, qual seu retorno e quantas vezes foi chamada realizando um mock', () => {
     service.randomNumber = jest.fn().mockImplementationOnce((num1, num2) => num1 / num2);
 
     expect(service.randomNumber(4, 2)).toBe(2);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  it('Verifica se a função randomNumber foi chamada com um mock implementado 3 parametros, qual seu retorno, quantas vezes foi chamada', () => {
+    service.randomNumber = jest.fn().mockImplementation((num1, num2, num3) => num1 * num2 * num3);
+
+    expect(service.randomNumber(5, 3, 2)).toBe(30);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  it('Verifica se a função randomNumber foi chamada com um mock implementado 1 parametro e retornando o dobro', () => {
+    service.randomNumber.mockReset();
+
+    service.randomNumber = jest.fn().mockImplementationOnce((num1) => num1 * 2);
+    expect(service.randomNumber(5)).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled();
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
   });
 });
