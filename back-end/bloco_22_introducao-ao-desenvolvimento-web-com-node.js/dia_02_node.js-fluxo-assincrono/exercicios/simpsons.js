a5e124a73097d5af1a0ff7bb4dd2c39dff9eea97
WBomfim <willianbomfim@hotmail.com>
2022-07-27 21:56:07 -0300

    Refactor/Feat: Refatorada a função que traz as informações do arquivo simpsons.json e criada a função que pesquisa o id de um personagem e retorna as informações encontradas.

--- back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_02_node.js-fluxo-assincrono/exercicios/simpsons.js
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_02_node.js-fluxo-assincrono/exercicios/simpsons.js
@@ -2,24 +2,41 @@ const fs = require('fs').promises;
 
 const arquivo = './simpsons.json';
 
-function showInformation(array) {
-  array.forEach(({ id, name }) => {
-    console.log(`${id} - ${name}`);
-  });
-}
+async function readFileAndShowInformation() {
+  const simpsons = await fs.readFile(arquivo, 'utf-8');
+  const simpsonsData = JSON.parse(simpsons);
 
-async function readFile() {
-  try{
-    const simpsons = await fs.readFile(arquivo, 'utf-8');
-    const simpsonsData = JSON.parse(simpsons);
-    showInformation(simpsonsData);
-  } catch(error) {
-    console.error(`Erro ao ler arquivo ${arquivo}: ${error.message}`);
+  if(!simpsonsData) {
+    throw new Error(`Erro ao ler arquivo`);
   }
+  
+  return simpsonsData;
 }
 
-function main() {
-  readFile();
+async function selectInformation(id) {
+  const simpsons = await fs.readFile(arquivo, 'utf-8');
+  const simpsonsData = JSON.parse(simpsons);
+  const getSimpson = simpsonsData.find(({ id: idSimpson }) => Number(idSimpson) === id);
+
+  if(!getSimpson) {
+    throw new Error(`id não encontrado`);
+  }
+
+  return`${getSimpson.id} - ${getSimpson.name}`;
+}
+
+async function main() {
+  try {
+    const dataSimpsons = await readFileAndShowInformation();
+    dataSimpsons.forEach(({ id, name }) => {
+      console.log(`${id} - ${name}`);
+    });
+
+    const selectedSimpson = await selectInformation(1);
+    console.log(selectedSimpson);
+  } catch(error) {
+    console.log(error);
+  }
 }
 
 main();
