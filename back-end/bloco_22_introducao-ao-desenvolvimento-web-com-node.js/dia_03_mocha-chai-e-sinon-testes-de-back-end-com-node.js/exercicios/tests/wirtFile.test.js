9b9a8c56ec70daaf05c10f8f6ff15d74cd26d20c
WBomfim <willianbomfim@hotmail.com>
2022-07-29 00:18:46 -0300

    Test: Criado o arquivo wirtFile.test com os testes para uma função que cria um arquivo recebendo como parametro o conteúdo e o nome do arquivo.

---
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_03_mocha-chai-e-sinon-testes-de-back-end-com-node.js/exercicios/tests/wirtFile.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+//const wirtFile = require('../src/writingFile');
+
+const NOME_ARQUIVO = 'teste.txt';
+const CONTEUDO_ARQUIVO = 'Conteudo do arquivo de teste';
+
+describe('Testa se a função wirtFile executa as operações corretamente', () => {
+  describe('Passando o nome do arquivo e conteúdo a ser salvo', () => {
+    it('Verifica se o conteúdo salvo é uma string', () => {
+      const data = wirtFile(NOME_ARQUIVO, CONTEUDO_ARQUIVO);
+      expect(data).to.be.a('string');
+    });
+
+    it('Verifica se o retorno da função é igual a "OK"', () => {
+      const data = wirtFile(NOME_ARQUIVO, CONTEUDO_ARQUIVO);
+      expect(data).to.be.equal('OK');
+    });
+  });
+});
