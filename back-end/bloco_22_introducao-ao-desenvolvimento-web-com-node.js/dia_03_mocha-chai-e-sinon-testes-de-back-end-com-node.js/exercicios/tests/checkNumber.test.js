eb57373ddac678acf58f1f8290acc39897653312
WBomfim <willianbomfim@hotmail.com>
2022-07-28 21:17:00 -0300

    Test: Adicionado no arquivo de teste o teste que verifica se a função lança um erro quando o parametro passado para a função não for um número.

--- back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_03_mocha-chai-e-sinon-testes-de-back-end-com-node.js/exercicios/tests/checkNumber.test.js
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_03_mocha-chai-e-sinon-testes-de-back-end-com-node.js/exercicios/tests/checkNumber.test.js
@@ -4,37 +4,49 @@ const { checkNumber } = require("../src/checkNumber")
 describe('Testa a função que verifica se o número é neutro, positivo ou negativo', () => {
   describe('Quando o número for 0', () => {
     it('Verifica se a saída é uma string', () => {
-      const output = checkNumber(0)
-      expect(output).to.be.a('string')
+      const output = checkNumber(0);
+      expect(output).to.be.a('string');
     });
 
     it('Verifica se a saída é "neutro"', () => {
-      const output = checkNumber(0)
-      expect(output).to.equal('neutro')
+      const output = checkNumber(0);
+      expect(output).to.equal('neutro');
     });
   });
 
   describe('Quando o número for maior que 0', () => {
     it('Verifica se a saída é uma string', () => {
-      const output = checkNumber(5)
-      expect(output).to.be.a('string')
+      const output = checkNumber(5);
+      expect(output).to.be.a('string');
     });
 
     it('Verifica se a saída é "positivo"', () => {
-      const output = checkNumber(5)
-      expect(output).to.equal('positivo')
+      const output = checkNumber(5);
+      expect(output).to.equal('positivo');
     });
   });
 
   describe('Quando o número for menor que 0', () => {
     it('Verifica se a saída é uma string', () => {
-      const output = checkNumber(-5)
-      expect(output).to.be.a('string')
+      const output = checkNumber(-5);
+      expect(output).to.be.a('string');
     });
 
     it('Verifica se a saída é "negativo"', () => {
-      const output = checkNumber(-5)
-      expect(output).to.equal('negativo')
+      const output = checkNumber(-5);
+      expect(output).to.equal('negativo');
     });
   });
+
+  describe('Quando o parametro passado para a função não é um número', () => {
+    it('Verifica se a saída é uma string', () => {
+      const output = checkNumber('teste');
+      expect(output).to.be.a('string');
+    });
+
+    it('Verifica se a função lança o erro "o valor deve ser um número"', () => {
+      const output = checkNumber('teste');
+      expect(output).to.throw('o valor deve ser um número');
+    })
+  })
 });
