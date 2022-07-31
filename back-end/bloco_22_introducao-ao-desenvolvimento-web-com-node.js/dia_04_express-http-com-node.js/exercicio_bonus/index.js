a3e5969b52b8e659c23190bd999fd78fcbfe9d34
WBomfim <willianbomfim@hotmail.com>
2022-07-31 02:45:06 -0300

    Feat: Criada a requisição com o endpoint /signup que recebe no body os dados a serem validados

--- back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_04_express-http-com-node.js/exercicio_bonus/index.js
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_04_express-http-com-node.js/exercicio_bonus/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const { authorization } = require('./authorization');
+const { generateToken } = require('./generateToken');
 
 const app = express();
+app.use(express.json());
 app.use(authorization);
 
 app.listen(3005, () => console.log('Ouvindo na porta 3005'));
+
+app.post('/signup', (req, res) => {
+  try{
+  const {email, password, firstName, phone} = req.body;
+  if (!email || !password || !firstName || !phone) {
+    return res.status(401).json({ message: 'missing fields' });
+  }
+  const token = generateToken();
+  return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).end();
+  }
+});
