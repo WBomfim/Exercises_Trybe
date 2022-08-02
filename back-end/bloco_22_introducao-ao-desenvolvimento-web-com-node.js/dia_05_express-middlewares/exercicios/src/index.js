e01add0f393be1ef85366caa497c0d99f0f11dda
WBomfim <willianbomfim@hotmail.com>
2022-08-02 02:24:19 -0300

    Feat: Inserido tratamento de erros nos exercícios 22.5

--- back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/index.js
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/index.js
@@ -5,6 +5,7 @@ const { validateInfos } = require('./middlewares/validateInfos');
 const { validateInfosSignup } = require('./middlewares/validateInfosSignup');
 const { generateToken } = require('../src/helpers/generateToken');
 const { validateToken } = require('./middlewares/validateToken');
+const { throwErrors } = require('../src/middlewares/throwErrors');
 
 const app = express();
 app.use(express.json());
@@ -13,10 +14,14 @@ app.post('/sales', validateToken, validateProductName, validateInfos, (_req, res
   return res.status(201).json({ message: 'Venda cadastrada com sucesso' })
 });
 
-app.post('/signup', validateInfosSignup, async (_req, res) => {
+app.post('/signup', validateInfosSignup, throwErrors(async (_req, res) => {
   const token = generateToken();
   await fs.writeFile('tokens.txt', token);
   return res.status(200).json({ token });
+}));
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ error: `Erro: ${err.message}` });
 });
 
 app.listen(3005, () => console.log('Ouvindo na porta 3005'));
--- back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/middlewares/validateToken.js
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/middlewares/validateToken.js
@@ -1,13 +1,16 @@
 const fs = require('fs/promises');
 
 async function validateToken(req, res, next) {
+  try {
   const token = await fs.readFile('tokens.txt', 'utf8');
   const { authorization } = req.headers;
   if (token !== authorization) {
     return res.status(401).send({ message: 'Token inválido!' });
   }
-  
   next();
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = {
