756454e560c2233f3e827ca59cc278c8af9c441a
WBomfim <willianbomfim@hotmail.com>
2022-08-02 01:15:10 -0300

    Feat: Inserido o middleware de verificação das informações da rota /signup do exercício 22.5

--- back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/index.js
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { validateProductName } = require('./middlewares/validateProductName');
 const { validateInfos } = require('./middlewares/validateInfos');
+const { validateInfosSignup } = require('./middlewares/validateInfosSignup');
+const { generateToken } = require('../src/helpers/generateToken');
 
 const app = express();
 app.use(express.json());
@@ -9,8 +11,9 @@ app.post('/sales', validateProductName, validateInfos, (_req, res) => {
   return res.status(201).json({ message: 'Venda cadastrada com sucesso' })
 });
 
-app.post('/signup', (_req, res) => {
-  return res.status(201).json({ message: 'ok' })
+app.post('/signup', validateInfosSignup, (_req, res) => {
+  const token = generateToken();
+  return res.status(200).json({ token });
 });
 
 app.listen(3005, () => console.log('Ouvindo na porta 3005'));
---
+++ back-end/bloco_22_introducao-ao-desenvolvimento-web-com-node.js/dia_05_express-middlewares/exercicios/src/middlewares/validateInfosSignup.js
@@ -0,0 +1,13 @@
+function validateInfosSignup(req, res, next) {
+  const { email, password, firstName, phone } = req.body;
+
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'missing fields' });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateInfosSignup
+}
