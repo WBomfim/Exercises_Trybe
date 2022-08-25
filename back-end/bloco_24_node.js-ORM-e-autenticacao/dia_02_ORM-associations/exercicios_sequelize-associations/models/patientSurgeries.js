ed4080dcb3956a5d736c39b11b1f2ed4c07e7b76
WBomfim <willianbomfim@hotmail.com>
2022-08-24 21:48:54 -0300

    feat: Criado o model PatientSurgeries

---
+++ back-end/bloco_24_node.js-ORM-e-autenticacao/dia_02_ORM-associations/exercicios_sequelize-associations/models/patientSurgeries.js
@@ -0,0 +1,25 @@
+const PatientSurgeries = (sequelize, DataTypes) => {
+  const PatientSurgeries = sequelize.define(
+    'PatientSurgeries',
+    {},
+    { timestamps: false }
+  );
+
+  PatientSurgeries.associate = (models) => {
+    models.Surgeries.belongsToMany(models.Patients, {
+      through: PatientSurgeries,
+      foreignKey: 'surgery_id',
+      otherKey: 'patient_id',
+    });
+
+    models.Patients.belongsToMany(models.Surgeries, {
+      through: PatientSurgeries,
+      foreignKey: 'patient_id',
+      otherKey: 'surgery_id',
+    });
+  };
+  
+  return PatientSurgeries;
+};
+
+module.exports = PatientSurgeries;
