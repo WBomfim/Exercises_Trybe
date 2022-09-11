35af1906038722da0ca347a5ee673b4694e9483c
WBomfim <willianbomfim@hotmail.com>
2022-09-10 23:59:30 -0300

    feat: Estendemos a interface Pizza para novos tipos de pizzas

--- back-end/bloco_26_typescript/dia_02_tipagem-estatica-e-generics/exercicios/src/Pizza.ts
+++ back-end/bloco_26_typescript/dia_02_tipagem-estatica-e-generics/exercicios/src/Pizza.ts
@@ -1,20 +1,55 @@
+import { type } from "os";
+
 type Slices = 4 | 6 | 8;
 
 interface Pizza {
-  flavor: string;
+  flavor: string | Comum | Vegetariana | Doce;
   slices: Slices;
 };
 
-const calabresa: Pizza = {
+const calabresa1: Pizza = {
   flavor: 'Calabresa',
   slices: 8
 };
-
 console.log(calabresa);
 
-const marguerita: Pizza = {
+const marguerita2: Pizza = {
   flavor: "Marguerita",
   slices: 6
 }
-
 console.log(marguerita);
+
+type Comum = 'calabresa' | 'frango' | 'pepperoni';
+type Vegetariana = 'marguerita' | 'palmito' | 'cogumelos';
+type Doce = 'nutela' | 'goiabada com queijo' | 'marshmallow';
+
+interface PizzaComum extends Pizza {
+  flavor: Comum;
+};
+
+interface PizzaVegetariana extends Pizza {
+  flavor: Vegetariana;
+};
+
+interface PizzaDoce extends Pizza {
+  flavor: Doce;
+};
+
+const calabresa2: PizzaComum = {
+  flavor:"calabresa",
+  slices: 8
+};
+console.log(calabresa2);
+
+
+const marguerita3: PizzaVegetariana = {
+  flavor: "marguerita",
+  slices: 6
+};
+console.log(marguerita3);
+
+const nutela: PizzaDoce = {
+  flavor: "nutela",
+  slices: 4
+};
+console.log(nutela);
