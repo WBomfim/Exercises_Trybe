ef44d05600bab84c364ed33e7c7c313ff7ad87b8
WBomfim <willianbomfim@hotmail.com>
2022-09-10 21:41:44 -0300

    feat: Criado a classe Car

--- back-end/bloco_26_typescript/dia_02_tipagem-estatica-e-generics/exercicios/src/Car.ts
+++ back-end/bloco_26_typescript/dia_02_tipagem-estatica-e-generics/exercicios/src/Car.ts
@@ -1,10 +1,53 @@
-enum color {
-    preto,
-    branco,
-    vermelho,
-    prata,
+import Colors from "./Colors";
+import Directions from "./Directions";
+import Doors from "./Doors";
+
+class Car {
+    brand: string;
+    color: Colors;
+    doors: number;
+
+    constructor(brand: string, color: Colors, doors: number) {
+        this.brand = brand;
+        this.color = color;
+        this.doors = doors;
+    }
+
+    turnOn(): void {
+      console.log("Carro ligado.");
+    }
+  
+    turnOff(): void {
+      console.log("Carro desligado.");
+    }
+  
+    turn(direction: Directions): void {
+      console.log(`Virando para a ${direction}.`);
+    }
+  
+    speedUp(): void {
+      console.log("Acelerando.");
+    }
+  
+    speedDown(): void {
+      console.log("Reduzindo a velocidade.");
+    }
+  
+    stop(): void {
+      console.log("Parando o carro.");
+    }
+  
+    honk(): void {
+      console.log("Buzinando: BIIIIIIIP!");
+    }
+  
+    openTheDoor(door: Doors): void {
+      console.log(`Abrindo a porta ${door}.`);
+    }
+  
+    closeTheDoor(door: Doors): void {
+      console.log(`Fechando a porta ${door}.`);
+    }
 }
 
-class car {
-
-}
+export default Car;
