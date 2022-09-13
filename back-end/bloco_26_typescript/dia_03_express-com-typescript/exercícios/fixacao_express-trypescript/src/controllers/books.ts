7feea442e827ebf570476b61e3156bab2cf1039b
WBomfim <willianbomfim@hotmail.com>
2022-09-12 23:00:47 -0300

    feat: Criado as funções e a rota para buscar um livro pelo id

--- back-end/bloco_26_typescript/dia_03_express-com-typescript/exercícios/fixacao_express-trypescript/src/controllers/books.ts
+++ back-end/bloco_26_typescript/dia_03_express-com-typescript/exercícios/fixacao_express-trypescript/src/controllers/books.ts
@@ -7,7 +7,16 @@ class BooksController {
 
   public getAll = async (_req: Request, res: Response) => {
     const books = await this.bookService.getAll();
-    res.status(StatusCodes.OK).json(books);
+    return res.status(StatusCodes.OK).json(books);
+  };
+
+  public getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const book = await this.bookService.getById(Number(id));
+    if (!book) {
+      return res.sendStatus(StatusCodes.NOT_FOUND).json({ message: 'Book not found!'});
+    }
+    return res.status(StatusCodes.OK).json(book);
   };
 }
 
