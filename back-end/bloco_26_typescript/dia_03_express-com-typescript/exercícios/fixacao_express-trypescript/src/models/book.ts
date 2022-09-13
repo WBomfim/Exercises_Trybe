59f0f7ee43de5948a4e149ec4b05b95e87f5ffd7
WBomfim <willianbomfim@hotmail.com>
2022-09-12 22:38:18 -0300

    feat: Criado o model book

---
+++ back-end/bloco_26_typescript/dia_03_express-com-typescript/exercícios/fixacao_express-trypescript/src/models/book.ts
@@ -0,0 +1,28 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Book from '../interfaces/book';
+
+export default class BookModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Book[]> {
+    const query = 'SELECT * FROM books';
+    const result = await this.connection.execute(query);
+    const [rows] = result;
+    return rows as Book[];
+  }
+
+  public async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+    const query = 'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)';
+    const result = await this.connection.execute<ResultSetHeader>(
+      query, [title, price, author, isbn]
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...book };
+  }
+}
