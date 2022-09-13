df47c75814c11ef76a51622b6dc3db4c9cb926d4
WBomfim <willianbomfim@hotmail.com>
2022-09-12 22:33:35 -0300

    feat: Criado a rota books e a conexão com o banco

--- back-end/bloco_26_typescript/dia_03_express-com-typescript/exercícios/fixacao_express-trypescript/src/index.ts
+++ back-end/bloco_26_typescript/dia_03_express-com-typescript/exercícios/fixacao_express-trypescript/src/index.ts
@@ -2,16 +2,19 @@ import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 import 'express-async-errors';
 import handleErrors from './middlewares/handleErrors';
+import booksRouter from './routes/books';
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = 8000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.status(StatusCodes.OK).send('Express + TypeScript')
 });
+
+app.use(booksRouter);
 
 app.use(handleErrors);
 
