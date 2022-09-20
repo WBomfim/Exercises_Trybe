2fdd533335ba496991ceb752a7f54f5166dfb8b1
WBomfim <willianbomfim@hotmail.com>
2022-09-20 00:21:32 -0300

    feat: Refatorado a classe Students para extender de Person e implementar Enrollable

--- back-end/bloco_27_programacao-orientada-a-objetos-(POO)-e-SOLID/dia_02_heranca-e-interfaces/exercicios/src/Student.ts
+++ back-end/bloco_27_programacao-orientada-a-objetos-(POO)-e-SOLID/dia_02_heranca-e-interfaces/exercicios/src/Student.ts
@@ -1,6 +1,7 @@
 import Person from "./Person";
+import Enrollable from "./Enrollable";
 
-export default class Student extends Person {
+export default class Student extends Person implements Enrollable {
   private _enrollment = String();
   private _examsGrades: number[] = [];
   private _worksGrades: number[] = [];
@@ -15,7 +16,9 @@ export default class Student extends Person {
   }
 
   set enrollment(value: string) {
-    if (value.length < 16) throw new Error('A matrícula deve possuir no mínimo 16 caracteres.');
+    if (value.length < 16) {
+      throw new Error('A matrícula deve possuir no mínimo 16 caracteres.')
+    }
 
     this._enrollment = value;
   }
@@ -25,7 +28,9 @@ export default class Student extends Person {
   }
 
   set examsGrades(value: number[]) {
-    if (value.length > 4) throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
+    if (value.length > 4) {
+      throw new Error('O aluno não pode ter mais que 4 notas em provas.')
+    }
 
     this._examsGrades = value;
   }
@@ -35,11 +40,28 @@ export default class Student extends Person {
   }
 
   set worksGrades(value: number[]) {
-    if (value.length > 2) throw new Error('A pessoa estudante só pode possuir 2 notas de trabalhos.');
+    if (value.length > 2) {
+      throw new Error('O aluno não pode ter mais que 2 notas em trabalhos.')
+    }
 
     this._worksGrades = value;
   }
 
+  sumGrades(): number {
+    const examsSum = this.examsGrades.reduce((acc, cur) => acc + cur);
+    const worksSum = this.worksGrades.reduce((acc, cur) => acc + cur);
+
+    return examsSum + worksSum;
+  }
+
+  sumAverageGrade(): number {
+    const sum = this.sumGrades();
+    const div = this.examsGrades.length + this.worksGrades.length;
+
+    return Math.round(sum / div);
+  }
+
+  
   generateEnrollment(): string {
     const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
 
