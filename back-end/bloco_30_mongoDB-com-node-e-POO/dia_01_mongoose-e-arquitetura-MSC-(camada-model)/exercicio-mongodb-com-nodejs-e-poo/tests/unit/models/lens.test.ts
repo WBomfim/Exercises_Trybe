a454d3f6b54e7b7786a21941380be00ea1e4bb86
WBomfim <willianbomfim@hotmail.com>
2022-10-20 22:57:10 -0300

    test: Criado testes para cobertura do model Lens

--- back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/tests/unit/models/lens.test.ts
+++ back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/tests/unit/models/lens.test.ts
@@ -1,9 +1,38 @@
 import { expect } from "chai";
 import Sinon from "sinon";
 import { Model }  from "mongoose";
-import { frameMock, frameMockWithId } from '../../mocks/frameMock';
+import { lensMock, lensMockWithId } from "../../mocks/lensMock";
 import LensModel from "../../../src/models/Lens";
 
 describe("Lens Model", () => {
   const lensModel = new LensModel();
+
+  before(() => {
+    Sinon.stub(Model, "create").resolves(lensMockWithId);
+    Sinon.stub(Model, "findOne").resolves(lensMockWithId);
+  });
+
+  after(() => Sinon.restore());
+
+  describe("creating a lens", () => {
+    it("successfully created", async () => {
+      const newLens = await lensModel.create(lensMock);
+      expect(newLens).to.be.deep.equal(lensMockWithId);
+    });
+  });
+
+  describe("searching a lens by id", () => {
+    it("successfully found", async () => {
+      const lensFound = await lensModel.readOne("62cf1fc6498565d94eba52cd");
+      expect(lensFound).to.be.deep.equal(lensMockWithId);
+    });
+
+    it("_id not found", async () => {
+      try {
+        await lensModel.readOne("123ERRADO");
+      } catch (error: any) {
+        expect(error.message).to.be.eq("InvalidMongoId");
+      }
+    });
+  });
 });
