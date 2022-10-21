0353b69f744d66c1fffe10a76cdff89eb4672da8
WBomfim <willianbomfim@hotmail.com>
2022-10-21 01:08:23 -0300

    feat: Criado as rotas para Frame

--- back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/src/app.ts
+++ back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
 import handleErrors from './middlewares/handleErrors';
+import frameRouter from './routes/frame';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(frameRouter);
 
 app.use(handleErrors);
 
