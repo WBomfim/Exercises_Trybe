4454e4e6a029257c01f1442d96224e16f1cc0a91
WBomfim <willianbomfim@hotmail.com>
2022-10-21 00:41:05 -0300

    feat: Criado um middleware para lidar com os erros da aplicação

--- back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/src/app.ts
+++ back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
+import handleErrors from './middlewares/handleErrors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(handleErrors);
 
 export default app;
