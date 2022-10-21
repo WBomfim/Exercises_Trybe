a90b6ff075dcf026c9cc414540b04772916b05a7
WBomfim <willianbomfim@hotmail.com>
2022-10-21 00:06:33 -0300

    feat: Criado a classe FrameService

---
+++ back-end/bloco_30_mongoDB-com-node-e-POO/dia_01_mongoose-e-arquitetura-MSC-(camada-model)/exercicio-mongodb-com-nodejs-e-poo/src/services/Frame.ts
@@ -0,0 +1,29 @@
+import IService from '../interfaces/Service';
+import IFrame, { FrameZodSchema } from '../interfaces/Frame';
+import IModel from '../interfaces/Model';
+import { ErrorTypes } from '../errors/catalog';
+
+class FrameService implements IService<IFrame> {
+  private _frame:IModel<IFrame>;
+  
+  constructor(model:IModel<IFrame>) {
+    this._frame = model;
+  }
+
+  public async create(obj:unknown):Promise<IFrame> {
+    const parsed = FrameZodSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return this._frame.create(parsed.data);
+  }
+
+  public async readOne(_id:string):Promise<IFrame> {
+    const frame = await this._frame.readOne(_id);
+    if (!frame) throw new Error(ErrorTypes.EntityNotFound);
+    return frame;
+  }
+}
+
+export default FrameService;
