db483ebdcc12f2ba045b6757722789a33c13cc05
WBomfim <willianbomfim@hotmail.com>
2022-12-20 18:51:51 -0300

    feat: Inserido os exercícios e aprendizados do bloco 34.3

---
+++ ciencia-da-computacao/bloco_34_padroes-de-projeto/dia_03_padroes-decorator-observer-factory/README.md
@@ -0,0 +1,7 @@
+# DIA 03 - Padrões - Decorator, Observer e Factory
+
+Hoje aprendi:
+
+- Utilizar os padrões **Decorator, Observer e Factory**, entendendo onde eles podem ser aplicados.
+- Identificar `Code Smells`.
+- Identificar e tratar `Data Clumps` e `Middle Man`.
---
+++ ciencia-da-computacao/bloco_34_padroes-de-projeto/dia_03_padroes-decorator-observer-factory/exercicios/despertador.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+
+
+class Alarme:
+    def __init__(self):
+        self.__rotinas = []
+
+    def adicionar_rotinas(self, rotina):
+        self.__rotinas.append(rotina)
+
+    def acionar_todas_rotinas(self):
+        for rotina in self.__rotinas:
+            rotina.acionar()
+
+    def despertar(self):
+        print("Alarme disparado")
+        self.acionar_todas_rotinas()
+
+
+# Observer
+class Acionador(ABC):
+    @abstractmethod
+    def acionar(self):
+        pass
+
+
+class AcionadorLuzes(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Acendendo as Luzes")
+
+
+class AcionadorCafeteira(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Preparando o café")
+
+
+class AcionadorComputador(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Ligar computador")
+
+
+if __name__ == "__main__":
+    alarme_manha = Alarme()
+    AcionadorLuzes(alarme_manha)
+    AcionadorCafeteira(alarme_manha)
+    AcionadorComputador(alarme_manha)
+
+    alarme_manha.despertar()
