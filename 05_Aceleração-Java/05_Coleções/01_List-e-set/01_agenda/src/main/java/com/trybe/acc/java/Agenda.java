a4b0fd835136ecf9681e63bffd303bf3fc578db7
WBomfim <willianbomfim@hotmail.com>
2023-03-09 13:10:22 -0300

    feat: Adicionado o exercício agenda de Java

---
+++ 05_Aceleração-Java/05_Coleções/01_List-e-set/01_agenda/src/main/java/com/trybe/acc/java/Agenda.java
@@ -0,0 +1,29 @@
+package com.trybe.acc.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class Agenda.
+ *
+ */
+public class Agenda {
+  private List<String> contatos = new ArrayList<>();
+
+  public void incluirContato(String contato) {
+    contatos.add(contato);
+  }
+
+  public void excluirContato(String contato) {
+    contatos.remove(contato);
+  }
+
+  public boolean verificaContato(String contato) {
+    return contatos.contains(contato);
+  }
+
+  public int tamanhoAgenda() {
+    return contatos.size();
+  }
+
+}
