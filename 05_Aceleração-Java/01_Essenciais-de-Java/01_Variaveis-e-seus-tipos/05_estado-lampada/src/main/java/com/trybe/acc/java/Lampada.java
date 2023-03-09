e8919c45e6640a063bd1f03cb45ccfa2a8086937
WBomfim <willianbomfim@hotmail.com>
2023-03-09 10:27:18 -0300

    feat: Adicionado exercício de Java estado-lampada

---
+++ 05_Aceleração-Java/01_Essenciais-de-Java/01_Variaveis-e-seus-tipos/05_estado-lampada/src/main/java/com/trybe/acc/java/Lampada.java
@@ -0,0 +1,18 @@
+package com.trybe.acc.java;
+
+/**
+ * Classe para ligar e desligar a lampada.
+ */
+public class Lampada {
+
+  /**
+   * Método para exibir o estado da lampada.
+   */
+  public static void main(String[] args) {
+    boolean isLigada = true;
+
+    System.out.println("A lampada ligada: " + isLigada + "!");
+
+  }
+
+}
