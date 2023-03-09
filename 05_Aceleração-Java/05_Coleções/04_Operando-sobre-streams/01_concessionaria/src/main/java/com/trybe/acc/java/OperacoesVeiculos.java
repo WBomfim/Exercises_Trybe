35d1dff0a7d751ed2bcf2b2a5d4ae37584543589
WBomfim <willianbomfim@hotmail.com>
2023-03-09 13:16:23 -0300

    feat: Adicionado o exercício concessionaria de Java

---
+++ 05_Aceleração-Java/05_Coleções/04_Operando-sobre-streams/01_concessionaria/src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
@@ -0,0 +1,25 @@
+package com.trybe.acc.java;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Class OperacoesVeiculos.
+ *
+ */
+public class OperacoesVeiculos {
+
+  public <T> int numVeiculosDiferentesVendidos(Stream<T> veiculosVendidos) {
+    return veiculosVendidos.distinct().toArray().length;
+  }
+
+  public <T> Stream<String> obterRepresentacao(Stream<T> veiculos) {
+    return veiculos.map(veiculo -> veiculo.toString());
+  }
+
+  public <T> List<T> obterLista(Stream<T> veiculos) {
+    return veiculos.collect(Collectors.toList());
+  }
+
+}
