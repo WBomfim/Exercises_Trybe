c2337da0ed4724c4b54509744f59453c6aab170b
WBomfim <willianbomfim@hotmail.com>
2023-03-09 10:32:41 -0300

    feat: Adicionado o exercício soma-elementos de Java

---
+++ 05_Aceleração-Java/01_Essenciais-de-Java/01_Variaveis-e-seus-tipos/07_soma-elementos/src/main/java/com/trybe/acc/java/SomaValores.java
@@ -0,0 +1,32 @@
+package com.trybe.acc.java;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Classe para soma de valores.
+ */
+public class SomaValores {
+
+  /**
+   * Executa a soma dos valores.
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+    int[] myArray = new int[3];
+
+    for (int index = 0; index < 3; index++) {
+      System.out.println("Digite um número:");
+      myArray[index] = scan.nextInt();
+    }
+
+    int soma = myArray[0] + myArray[2];
+
+
+    System.out.println("A soma é: " + soma);
+
+    scan.close();
+  }
+
+}
