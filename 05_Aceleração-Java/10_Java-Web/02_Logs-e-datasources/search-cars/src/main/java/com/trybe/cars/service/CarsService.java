d11f61192c6a8b50af861a6175ce583bc9036210
WBomfim <willianbomfim@hotmail.com>
2023-03-16 14:30:09 -0300

    feat: Inserido exercício search-cars do módulo de Java

---
+++ 05_Aceleração-Java/10_Java-Web/02_Logs-e-datasources/search-cars/src/main/java/com/trybe/cars/service/CarsService.java
@@ -0,0 +1,22 @@
+package com.trybe.cars.service;
+
+import com.trybe.cars.model.Car;
+import com.trybe.cars.repository.CarsRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Classe CarsService.
+ * 
+ */
+@Service
+public class CarsService {
+  @Autowired
+  private CarsRepository carRepository;
+
+  public List<Car> getAllCars() {
+    return carRepository.findAll();
+  }
+
+}
