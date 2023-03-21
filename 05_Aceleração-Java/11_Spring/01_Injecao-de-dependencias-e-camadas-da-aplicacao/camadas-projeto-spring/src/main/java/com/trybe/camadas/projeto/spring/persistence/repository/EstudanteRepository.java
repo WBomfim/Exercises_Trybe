e07c50ea34441f255e039f3a475d2a7d54e7a83c
WBomfim <willianbomfim@hotmail.com>
2023-03-21 17:23:55 -0300

    feat: Inserido exercício camadas-projeto-spring de Java

---
+++ 05_Aceleração-Java/11_Spring/01_Injecao-de-dependencias-e-camadas-da-aplicacao/camadas-projeto-spring/src/main/java/com/trybe/camadas/projeto/spring/persistence/repository/EstudanteRepository.java
@@ -0,0 +1,17 @@
+package com.trybe.camadas.projeto.spring.persistence.repository;
+
+import com.trybe.camadas.projeto.spring.persistence.entity.Estudante;
+
+/**
+ * Interface EstudanteRepository.
+ *
+ */
+public interface EstudanteRepository {
+  public Estudante criarEstudante(Estudante estudante);
+
+  public Estudante buscaEstudantepeloId(String id);
+
+  public Estudante atualizaEstudantepeloId(String id, Estudante estudante);
+
+  public void excluiEstudantepeloId(String id);
+}
---
+++ 05_Aceleração-Java/11_Spring/01_Injecao-de-dependencias-e-camadas-da-aplicacao/camadas-projeto-spring/src/main/java/com/trybe/camadas/projeto/spring/persistence/repository/EstudanteRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.trybe.camadas.projeto.spring.persistence.repository;
+
+import com.trybe.camadas.projeto.spring.persistence.entity.Estudante;
+
+/**
+ * Class EstudanteRepositoryImpl.
+ *
+ */
+public class EstudanteRepositoryImpl implements EstudanteRepository {
+  @Override
+  public Estudante criarEstudante(Estudante estudante) {
+    return null;
+  }
+
+  @Override
+  public Estudante buscaEstudantepeloId(String id) {
+    return null;
+  }
+
+  @Override
+  public Estudante atualizaEstudantepeloId(String id, Estudante estudante) {
+    return null;
+  }
+
+  @Override
+  public void excluiEstudantepeloId(String id) {
+  }
+}
