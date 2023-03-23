0b800a8dca857f6118db3bf74a2cf99f73c3a544
WBomfim <willianbomfim@hotmail.com>
2023-03-23 16:41:40 -0300

    feat: Realizado o exercício pedro-quer-viajar

---
+++ 05_Aceleração-Java/11_Spring/03_Rest-sql-e-nosql/pedro-quer-viajar/src/main/java/com/trybe/acc/java/planejadorviagens/challenge/PlanejandoDestino.java
@@ -0,0 +1,54 @@
+package com.trybe.acc.java.planejadorviagens.challenge;
+
+import com.trybe.acc.java.planejadorviagens.model.Country;
+import com.trybe.acc.java.planejadorviagens.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class PlanejandoDestino.
+ * 
+ */
+@Component
+public class PlanejandoDestino {
+  private String[] paises = new String[] {"BRASIL", "ARGENTINA", "URUGUAI", "PANAMA"};
+  private String[] capitais =
+      new String[] {"BRASILIA", "BUENOS AIRES", "MONTEVIDEO", "CIDADE DO PANAMA"};
+  private String[] ddis = new String[] {"+55", "+54", "+598", "+507"};
+
+  @Autowired
+  private CountryRepository countryRepository;
+
+  /**
+   * Method to insert a new country.
+   */
+  public void inserir() {
+    for (int i = 0; i < paises.length; i += 1) {
+      Country country = new Country();
+      country.setName(paises[i]);
+      country.setCapital(capitais[i]);
+      countryRepository.save(country);
+    }
+  }
+
+  /**
+   * Method to update a country.
+   *
+   */
+  public void corrigir() {
+    for (int i = 0; i < paises.length; i += 1) {
+      Country country = countryRepository.findByName(paises[i]).get(0);
+      country.setDdi(ddis[i]);
+      countryRepository.save(country);
+    }
+  }
+
+  /**
+   * Method to delete a country.
+   * 
+   */
+  public void excluir() {
+    Country country = countryRepository.findByName(paises[paises.length - 1]).get(0);
+    countryRepository.delete(country);
+  }
+}
---
+++ 05_Aceleração-Java/11_Spring/03_Rest-sql-e-nosql/pedro-quer-viajar/src/main/java/com/trybe/acc/java/planejadorviagens/repository/CountryRepository.java
@@ -0,0 +1,18 @@
+package com.trybe.acc.java.planejadorviagens.repository;
+
+import com.trybe.acc.java.planejadorviagens.model.Country;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repositório para a entidade Country.
+ * 
+ */
+@Repository
+public interface CountryRepository extends MongoRepository<Country, String> {
+
+  List<Country> findByName(String name);
+  
+}
