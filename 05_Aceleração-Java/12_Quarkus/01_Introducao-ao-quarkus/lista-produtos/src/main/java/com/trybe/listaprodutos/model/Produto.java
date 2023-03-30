fdaeb2c9f0eb8192c7b5881474546f15ff1c78fa
WBomfim <willianbomfim@hotmail.com>
2023-03-30 15:33:50 -0300

    feat: Realizado o exercício lista-produtos de Quarkus

---
+++ 05_Aceleração-Java/12_Quarkus/01_Introducao-ao-quarkus/lista-produtos/src/main/java/com/trybe/listaprodutos/model/Produto.java
@@ -0,0 +1,27 @@
+package com.trybe.listaprodutos.model;
+
+/**
+ * Classe Produto.
+ * 
+ */
+public class Produto {
+
+  private int id;
+  private String nome;
+
+  public Produto() {}
+
+  public Produto(int id, String nome) {
+    this.id = id;
+    this.nome = nome;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+}
